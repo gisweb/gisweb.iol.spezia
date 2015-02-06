d5807fc703e33958a6c4fd1c2cdc0a6221982a38
mamogmx <marco.carbone@gmx.com>
2015-02-06 09:25:31 +0100

    First Commit

---
+++ gisweb/iol/spezia/browser/views.py
@@ -0,0 +1,10 @@
+from Products.CMFCore.utils import getToolByName
+from plone.dexterity.browser.view import DefaultView
+from plone import api
+
+from zope.component import getUtility
+from gisweb.iol.spezia.interfaces import IolDocument
+from five import grok
+from Products.CMFPlomino.interfaces import IPlominoDocument
+
+
